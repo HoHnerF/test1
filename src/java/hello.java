74b80fed3c32d14de81d648fb5838029d16e2714
gwj <17365970352@qq.com>
2020-06-14 19:58:09 +0800

    wesfds

---
+++ src/java/hello.java
@@ -0,0 +1,16 @@
+package java;
+
+import java.sql.SQLOutput;
+
+/**
+ * @BelongsProject: he
+ * @BelongsPackage: java
+ * @Author: gwj
+ * @CreateTime: 2020-06-14 19:57
+ * @Description: todo
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println(":s");
+    }
+}
