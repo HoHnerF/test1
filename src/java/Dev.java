605b3c5f67a0ee1b1d70afa0d4ea0faedf7f2a0a
gwj <17365970352@qq.com>
2020-06-14 20:13:11 +0800

    dev

---
+++ src/java/Dev.java
@@ -0,0 +1,16 @@
+package java;
+
+/**
+ * @BelongsProject: he
+ * @BelongsPackage: java
+ * @Author: gwj
+ * @CreateTime: 2020-06-14 20:12
+ * @Description: todo
+ */
+public class Dev {
+    public static void main(String[] args) {
+
+        System.out.println("sdsd");
+    }
+
+}
