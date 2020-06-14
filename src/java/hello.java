c6e60dfaa692366fc88ba893e3377af8684de7c0
gwj <17365970352@qq.com>
2020-06-14 20:08:05 +0800

    wesfds

--- src/java/hello.java
+++ src/java/hello.java
@@ -10,5 +10,7 @@ package java;
 public class hello {
     public static void main(String[] args) {
         System.out.println(":s");
+        System.out.println(":s");
+        System.out.println(":s");
     }
 }
